9f251b4f5735a17596256b46e567a78a95b17280
kamilgrzechulski <kamil.grzechulskii@gmail.com>
2017-07-30 16:32:39 +0200

    config files for 'uer profile'

--- config/app.php
+++ config/app.php
@@ -163,6 +163,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Collective\Html\HtmlServiceProvider::class,
+        MrEssex\LaravelAuthProfile\AuthProfileServiceProvider::class,
 
         /*
          * Package Service Providers...
--- resources/views/layouts/template/body.blade.php
+++ resources/views/layouts/template/body.blade.php
@@ -206,7 +206,7 @@
                     <i class="fa fa-user fa-fw"></i> <i class="fa fa-caret-down"></i>
                 </a>
                 <ul class="dropdown-menu dropdown-user">
-                    <li><a href="#"><i class="fa fa-user fa-fw"></i> User Profile</a>
+                    <li><a href="{{ route('profile') }}"><i class="fa fa-user fa-fw"></i> User Profile</a>
                     </li>
                     <li><a href="{{ route('settings') }}"><i class="fa fa-gear fa-fw"></i> Settings</a>
                     </li>
